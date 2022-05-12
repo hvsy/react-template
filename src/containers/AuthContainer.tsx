dbbc5618626bb9213062df3f3647284c66352cbb
hvsy <sumenghua@live.com>
2022-05-12 15:43:05 +0800

    1. 更新AuthContainer, 支持获取当前用户数据

--- src/containers/AuthContainer.tsx
+++ src/containers/AuthContainer.tsx
@@ -1,21 +1,50 @@
-import {ReactNode, useEffect, useState} from "react";
+import {createContext, ReactNode, useContext, useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {AxiosInstance} from "@/lib/api";
 import _ from "lodash";
+import useSWR from "swr";
+import {LoadingOutlined} from "@ant-design/icons";
 
 export type AuthContainerProps = {
     redirect : string;
+    user : string;
     children : ReactNode,
+    
 };
 
-export function AuthContainer(props : AuthContainerProps ){
-    const {redirect} = props;
+
+const UserContext = createContext<{user:User|null}>({
+    user : null,
+});
+
+export function useUser(){
+    return useContext(UserContext).user!;
+}
+
+
+export function UserProvider(props : {url : string,children : ReactNode}){
+    const {url,children} = props;
+    const {data} = useSWR(url);
+    if(!data){
+        return <div className={'flex-1 flex flex-col justify-center items-center'}>
+            <LoadingOutlined />
+        </div>;
+    }
+    return <UserContext.Provider value={{user:data}}>
+        {children}
+    </UserContext.Provider>
+}
+
+
+export function AuthContainer(props : AuthContainerProps){
+    const {redirect,user} = props;
     const nav =useNavigate();
     const [mounted,setMounted] = useState(false);
     useEffect(() => {
         const interceptor = AxiosInstance.interceptors.response.use(r=>r, (error) => {
+            const isRedirect = _.get(error,'config.authRedirect',true);
             const status = _.get(error,'response.status');
-            if(status === 401){
+            if(status === 401 && isRedirect){
                 nav(redirect);
                 return;
             }
@@ -28,5 +57,7 @@ export function AuthContainer(props : AuthContainerProps ){
         }
     },[]);
     if(!mounted) return null;
-    return props.children
+    return <UserProvider url={user}>
+        {props.children}
+    </UserProvider>
 }
