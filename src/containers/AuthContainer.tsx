5cebc41c0714b7acc6afc1a86209ef3de161a6b7
hvsy <sumenghua@live.com>
2022-06-27 14:34:55 +0800

    1. useUser添加refresh方法 2. 添加CurrentUser组件

--- src/containers/AuthContainer.tsx
+++ src/containers/AuthContainer.tsx
@@ -2,35 +2,40 @@ import {createContext, ReactNode, useContext, useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {AxiosInstance} from "@/lib/api";
 import _ from "lodash";
-import useSWR from "swr";
+import useSWR, { KeyedMutator } from "swr";
 import {LoadingOutlined} from "@ant-design/icons";
 
 export type AuthContainerProps = {
     redirect : string;
     user : string;
     children : ReactNode,
-    
+
 };
 
-
-const UserContext = createContext<{user:User|null}>({
+export type UserContextValues = {
+    user:User|null;
+    refresh : (KeyedMutator<any>)|null;
+};
+const UserContext = createContext<UserContextValues>({
     user : null,
+    refresh : null,
 });
 
 export function useUser(){
-    return useContext(UserContext).user!;
+    const ctx= useContext(UserContext)!;
+    return [ctx.user!,ctx.refresh!] as const;
 }
 
 
 export function UserProvider(props : {url : string,children : ReactNode}){
     const {url,children} = props;
-    const {data} = useSWR(url);
+    const {data,mutate} = useSWR(url);
     if(!data){
         return <div className={'flex-1 flex flex-col justify-center items-center'}>
             <LoadingOutlined />
         </div>;
     }
-    return <UserContext.Provider value={{user:data}}>
+    return <UserContext.Provider value={{user:data,refresh : mutate}}>
         {children}
     </UserContext.Provider>
 }
---
+++ src/ui/fragments/CurrentUser.tsx
@@ -0,0 +1,51 @@
+import {FC} from "react";
+import {UserOutlined} from "@ant-design/icons";
+import {Button, Dropdown, Menu} from "antd";
+import {useUser} from "@/containers/AuthContainer";
+import {api} from "@/lib/api";
+import {useNavigate} from "react-router-dom";
+import {Token} from "@/lib/token";
+import {ItemType} from "antd/es/menu/hooks/useItems";
+
+export type CurrentUserProps = {
+    renderName : (user : User)=> React.ReactNode;
+    menus ?: ItemType[];
+};
+
+export function defaultRenderUserName(user : User){
+    return user.name;
+}
+export const CurrentUser: FC<CurrentUserProps> = (props) => {
+    const {menus = [],renderName = defaultRenderUserName} = props;
+    const [user,refresh] = useUser();
+    const nav = useNavigate();
+    return <Dropdown  overlay={<Menu
+            items={[
+                ...menus,
+                {
+                    label: '退出',
+                    key : 'logout'
+                }
+            ]}
+            onClick={(which) => {
+        switch(which.key){
+            case 'logout':
+            {
+                api({
+                    method: 'post',
+                    url: "/api/admin/logout",
+                }).then(() => {
+                    Token.remove();
+                    refresh(null);
+                    nav('/login');
+                });
+            }
+            break;
+        }
+    }} />
+    }>
+        <Button type="text" className="cursor-pointer" size={'small'} icon={<UserOutlined />}>
+            {renderName(user)}
+        </Button>
+    </Dropdown>;
+}
