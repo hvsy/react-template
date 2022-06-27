b12b641d2cc4b482bf03f8f7248e579aae09301d
hvsy <sumenghua@live.com>
2022-06-27 11:33:36 +0800

    1. 添加AppStorage类,用于处理Local Storage 2. 添加DeviceId 用于获取设备Id 3. 默认api请求withCredentials 为false 4. 如果远程服务器无法访问或者网络问题,则在console输出错误信息

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -8,7 +8,7 @@ export const AxiosInstance = axios.create({
         'X-Requested-With': 'XMLHttpRequest',
         'Accept' : 'application/json',
     },
-    withCredentials: true,
+    withCredentials: false,
 });
 AxiosInstance.interceptors.request.use((config) => {
     const token = Token.get();
@@ -19,12 +19,14 @@ AxiosInstance.interceptors.request.use((config) => {
 });
 AxiosInstance.interceptors.response.use(r=>r, (error) => {
     const statusCode = _.get(error,'response.status');
-    if(statusCode === 401){
+    if(!statusCode && _.get(error,'message') === 'Network Error'){
+        console.error('无法访问:',error.config.baseURL + error.config.url);
+    }else if(statusCode === 401){
         Token.remove();
     }
     return Promise.reject(error);
 });
-export function api<T>(config : AxiosRequestConfig){
+export function api<T>(config : AxiosRequestConfig & {authRedirect ?: boolean}){
     return AxiosInstance.request<T>(config).then(r=>r.data);
 }
 
--- src/lib/token.ts
+++ src/lib/token.ts
@@ -1,21 +1,2 @@
-class StorageToken{
-    key : string;
-    constructor(key : string){
-        this.key = key;
-    }
-    get(){
-        return localStorage.getItem(this.key);
-    }
-    remove(){
-        localStorage.removeItem(this.key);
-        return this;
-    }
-    set(token : string){
-        localStorage.setItem(this.key,token);
-        return this;
-    }
-    static factory(key : string){
-        return new StorageToken(key);
-    }
-}
-export const Token = StorageToken.factory(`__${process.env.APP_NAME}_APP_TOKEN__`);
+import {AppStorage} from "@/lib/AppStorage";
+export const Token = new AppStorage(`TOKEN`);
